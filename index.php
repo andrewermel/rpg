a73e0fbcd684d12a17d56bfe72d2e66cd3e970de
andrewermel <andrew.ermel@gmail.com>
2020-04-23 21:45:52 -0300

    projeto rpg

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="estilo.css">
+</head>
+<body>
+    <div class="jogador" id="j1">  </div>
+    <div class="jogador" id="j2">  </div>
+    <div class="jogador" id="j3">  </div>
+    <div class="jogador" id="j4">  </div>
+    <div class="jogador" id="j5">  </div>
+    <button  class="j1"> + </button>
+    <button  class="j1"> - </button>
+    <button  class="j2"> + </button>
+    <button  class="j2"> - </button>
+    <button  class="j3"> + </button>
+    <button  class="j3"> - </button>
+    <button  class="j4"> + </button>
+    <button  class="j4"> - </button>
+    
+    
+</body>
+</html>
